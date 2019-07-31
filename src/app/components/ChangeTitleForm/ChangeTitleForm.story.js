b565d8847c01c629cea157beed384c7d2e868f5d
Vũ Đức Duy <duyisking1060@gmail.com>
2019-07-31 20:31:44 +0700

    Finish setup

--- src/app/components/ChangeTitleForm/ChangeTitleForm.story.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { ChangeTitleForm } from './ChangeTitleForm';
-
-storiesOf('ChangeTitleForm', module)
-    .add('Initial', () => (
-        <ChangeTitleForm />
-    ));
