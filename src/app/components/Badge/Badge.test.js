93eeafa212e5556b41a6effe4faae108893cbcea
Vũ Đức Duy <duyisking1060@gmail.com>
2019-07-31 20:17:07 +0700

    Badge

---
+++ src/app/components/Badge/Badge.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Badge from './Badge';
+
+describe('Badge', () => {
+    const wrapper = shallow(<Badge />);
+
+    it('', () => {
+        //
+    });
+});
