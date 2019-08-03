a290ff0e62b70ae02252850d6eb00b2ec371088d
duy <duy>
2019-08-03 17:39:09 +0700

    duy_badge_2/8/2019

--- src/app/components/Badge/Badge.test.js
+++ src/app/components/Badge/Badge.test.js
@@ -4,7 +4,6 @@ import { shallow } from 'enzyme';
 import Badge from './Badge';
 
 describe('Badge', () => {
-    // eslint-disable-next-line no-unused-vars
     const wrapper = shallow(<Badge />);
 
     it('', () => {
