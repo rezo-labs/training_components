e0d7b56c1a134f916f7ed7d52bdc25e4aeaed2d0
duy <duy>
2019-08-03 19:25:30 +0700

    edit my .storybook

--- src/app/components/Badge/Badge.story.js
+++ src/app/components/Badge/Badge.story.js
@@ -5,8 +5,7 @@ import Badge from './Badge';
 storiesOf('Badge', module)
     .add('Initial', () => (
         <>
-        <Badge clickable variant='primary' size='medium' maxlength={100} >1</Badge>
-        
-        <Badge clickable variant='primary' size='large' maxlength={100} >1</Badge>
+            <Badge clickable variant="primary" size="small" maxlength={100}>1</Badge>
+            <Badge clickable variant="primary" size="medium" maxlength={100}>1</Badge>
         </>
     ));
