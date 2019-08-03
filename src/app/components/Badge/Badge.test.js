aa9580c24761c48cd882e6d0522414a26e0a4d98
duy <duy>
2019-08-03 19:47:44 +0700

    update Badge.test.js

--- src/app/components/Badge/Badge.test.js
+++ src/app/components/Badge/Badge.test.js
@@ -4,8 +4,16 @@ import { shallow } from 'enzyme';
 import Badge from './Badge';
 
 describe('Badge', () => {
+<<<<<<< HEAD
     it('Render as expected', () => {
         const wrapper = shallow(<Badge variant="primary" size="small" />);
         expect(wrapper.find('*')).not.toHaveLength(0);
+=======
+    // eslint-disable-next-line no-unused-vars
+    const wrapper = shallow(<Badge variant="primary" size="small"> Primary</Badge>);
+
+    it('', () => {
+        //
+>>>>>>> duynguyen_badge_2/8/2019
     });
 });
