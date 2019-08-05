a570899e7756cba3c812dc403e124474ed9f5e65
Vũ Đức Duy <duyisking1060@gmail.com>
2019-08-05 21:16:20 +0700

    Add Avatar component

---
+++ src/app/components/Avatar/Avatar.md
@@ -0,0 +1,5 @@
+Example:
+```jsx
+<Avatar name="D" status="online" size="small" />
+<Avatar clickable name="D" status="online" size="small" />
+```
---
+++ src/app/components/Avatar/Avatar.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+
+import Avatar from './Avatar';
+
+describe('Avatar', () => {
+    it('Render as expected', () => {
+        const wrapper = shallow(<Avatar name="D" status="online" size="small" />);
+        expect(wrapper.find('*')).not.toHaveLength(0);
+    });
+    it('Clickable', () => {
+        const wrapper = shallow(<Avatar clickable name="D" status="online" size="small" />);
+        expect(wrapper.find('*')).not.toHaveLength(0);
+    });
+    it('Should throw errors', () => {
+        console.error = jest.fn();
+        shallow(<Avatar name="D" status="oline" size="small" />);
+        shallow(<Avatar name="D" status="online" size="sall" />);
+        expect(console.error).toHaveBeenCalledTimes(2);
+    });
+    it('Call function on click', () => {
+        const mockOnClick = jest.fn();
+        const wrapper = mount(<Avatar name="D" status="online" size="small" onClick={mockOnClick} />);
+        wrapper.find('img').first().simulate('click');
+        expect(mockOnClick).toHaveBeenCalledTimes(1);
+    });
+});
