1ac676398aedec875ca360db02faa07c28e1054e
Loc Tran <39623488+loctran1902apcs@users.noreply.github.com>
2019-08-12 15:44:20 +0700

    Revert "[InputText] Update code "

--- src/app/components/InputText/InputText.story.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import InputText from './InputText';
-
-storiesOf('InputText', module)
-    .add('Initial', () => (
-        <InputText />
-    ));
